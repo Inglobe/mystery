693363de2035c3b8d1ea6b84acf548f4021ecdc1
Ignacio <nacho22martin@gmail.com>
2014-11-27 17:43:02 -0300

    change in configuration frozo db

--- sistema/includes/config.php
+++ sistema/includes/config.php
@@ -12,8 +12,8 @@ $host_db="localhost";
 $url_site="http://www.mysteryshopper.com.ar";
 $url_site_zas = "http://www.mysteryshopper.com.ar";
 
-$user_db_ferozo = "root";
+/*$user_db_ferozo = "root";
 $pass_db_ferozo = "9L7SFSir6e";
 $db_nombre_ferozo = "ferozo";
-$host_db_ferozo = "localhost";
+$host_db_ferozo = "localhost";*/
 ?>
